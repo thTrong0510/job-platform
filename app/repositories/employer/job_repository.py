757706f217fb7c683a4e0bfa29246f36dceb3fab
thTrong0510 <vthanhtrongng@gmail.com>
2026-04-30 21:17:18 +0700

    done: feature/JHQ-26

--- app/repositories/employer/job_repository.py
+++ app/repositories/employer/job_repository.py
@@ -1,4 +1,7 @@
-from app.models.skill import JobSkill, Skill
+from datetime import datetime
+
+from app.models.application import (Application)
+from app.models.skill import JobSkill
 from app.models.job import Job
 from app.extensions import db
 from sqlalchemy import or_
@@ -54,3 +57,63 @@ class JobRepository:
     @staticmethod
     def count_open_by_employer(employer_id):
         return Job.query.filter_by(employer_id=employer_id, status="OPEN").count()
+
+
+    @staticmethod
+    def count_applicants(job_id):
+        return db.session.query(Application).filter_by(job_id=job_id).count()
+
+
+    @staticmethod
+    def update_status(job_id, status):
+        job = Job.query.get(job_id)
+        if job:
+            job.status = status
+            db.session.commit()
+
+
+    @staticmethod
+    def delete(job_id):
+        job = Job.query.get(job_id)
+        if job:
+            db.session.delete(job)
+            db.session.commit()
+
+
+    @staticmethod
+    def find_by_id_and_employer(job_id, employer_id):
+        return Job.query.filter_by(id=job_id, employer_id=employer_id).first()
+
+    @staticmethod
+    def update(job_id, employer_id, form_data, skill_list):
+        try:
+            job = Job.query.filter_by(id=job_id, employer_id=employer_id).first()
+            if not job:
+                return None, "Không tìm thấy tin tuyển dụng."
+
+            # Cập nhật thông tin cơ bản
+            job.title = form_data.get('title')
+            job.location = form_data.get('location')
+            job.description = form_data.get('description')
+            job.salary_min = form_data.get('salary_min') or None
+            job.salary_max = form_data.get('salary_max') or None
+            job.experience_required = int(form_data.get('experience_required', 0))
+
+            if form_data.get('end_date'):
+                job.end_date = datetime.strptime(form_data.get('end_date'), '%Y-%m-%d')
+
+            # Xử lý Skills: Xóa các bản ghi cũ trực tiếp qua Query để đạt hiệu suất tốt nhất
+            JobSkill.query.filter_by(job_id=job_id).delete()
+
+            # Thêm các bản ghi mới
+            if skill_list:
+                for s_id in skill_list:
+                    new_js = JobSkill(job_id=job_id, skill_id=int(s_id.get("skill_id")))
+                    db.session.add(new_js)
+
+            db.session.commit()
+            return job, None
+
+        except Exception as e:
+            db.session.rollback()
+            return None, str(e)
