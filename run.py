47403e265849248385cd044bb0f4d517d49962a9
thTrong0510 <vthanhtrongng@gmail.com>
2026-05-10 17:57:50 +0700

    config api gg login to deploy v2

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 if __name__ == "__main__":
     app.run(debug=True)
